bcdbaea0094f9b164d586c0f71aa3c8b391af4b7
saidugurlu <ugurlumsaid@gmx.de>
2022-06-23 10:24:17 +0200

    initial commit and cleaned up

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import './App.scss'
+
+function App() {
+  const [jobs, setJobs] = useState([]);
+  return (
+    <div className="App">
+      <h1>Jobs</h1>
+      <p>There are {jobs.length} jobs.</p>
+    </div>
+  )
+}
+
+export default App
