118169f7d3119152d924bf41519d0de93a9c326d
saidugurlu <ugurlumsaid@gmx.de>
2022-06-23 10:28:03 +0200

    back/front works

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-
 import './App.scss'
+
+const backend_url = 'http://localhost:3033/jobs';
 
 function App() {
   const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      setJobs((await axios.get(backend_url)).data);
+    })();
+  }, []);
+
   return (
     <div className="App">
       <h1>Jobs</h1>
@@ -12,5 +20,4 @@ function App() {
     </div>
   )
 }
-
 export default App
